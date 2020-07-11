2cd25dbfe19d24c86b69d5da9af15cee798fd714
Dan Hilderbrand <16111574+danh955@users.noreply.github.com>
2020-07-12 08:20:23 +0900

    add chart list control

---
+++ HilresStock/Views/Chart/Stock.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="Stock.cs" company="Hilres">
+// Copyright (c) Hilres. All rights reserved.
+// </copyright>
+
+namespace HilresStock.Views.Chart
+{
+    /// <summary>
+    /// Stock record class.
+    /// </summary>
+    public class Stock
+    {
+        /// <summary>
+        /// Gets or sets iD.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets symbol.
+        /// </summary>
+        public string Symbol { get; set; }
+    }
+}
---
+++ HilresStock/Views/Chart/StockListControl.xaml.cs
@@ -0,0 +1,36 @@
+﻿// <copyright file="StockListControl.xaml.cs" company="Hilres">
+// Copyright (c) Hilres. All rights reserved.
+// </copyright>
+
+namespace HilresStock.Views.Chart
+{
+    using System.Collections.Generic;
+    using Windows.UI.Xaml.Controls;
+
+    /// <summary>
+    /// Stock list control class.
+    /// </summary>
+    public sealed partial class StockListControl : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StockListControl"/> class.
+        /// </summary>
+        public StockListControl()
+        {
+            this.InitializeComponent();
+            var stocks = new List<Stock>();
+
+            for (int i = 1; i < 25; i++)
+            {
+                stocks.Add(new Stock { Id = i, Symbol = i.ToString() });
+            }
+
+            this.Stocks = stocks;
+        }
+
+        /// <summary>
+        /// Gets list of stocks.
+        /// </summary>
+        public List<Stock> Stocks { get; }
+    }
+}
